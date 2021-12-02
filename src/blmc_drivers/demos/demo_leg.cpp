42055d7810b8a3be421881c779790731f51fcb51
Dawson <horvath.dawson@gmail.com>
2021-12-02 02:52:36 -0800

    added the demos into blmc_drivers for debugging... Stuff builds

--- src/blmc_drivers/demos/demo_leg.cpp
+++ src/blmc_drivers/demos/demo_leg.cpp
@@ -5,9 +5,9 @@
  */
 
 #include <math.h>
-#include <blmc_drivers/devices/analog_sensor.hpp>
-#include <blmc_drivers/devices/leg.hpp>
-#include <blmc_drivers/devices/motor.hpp>
+#include <monopod_sdk/blmc_drivers/devices/analog_sensor.hpp>
+#include <monopod_sdk/blmc_drivers/devices/leg.hpp>
+#include <monopod_sdk/blmc_drivers/devices/motor.hpp>
 #include "real_time_tools/spinner.hpp"
 #include "real_time_tools/timer.hpp"
 
