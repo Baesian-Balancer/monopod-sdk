5b47737a36ac6fc6a68deeee6f0c3ce9425c9da4
Dawson Horvath <horvath.dawson@gmail.com>
2022-01-24 02:42:47 -0800

    moved mode out of common header into its own header so that it can be be used by swig in scenario

--- include/monopod_sdk/common_header.hpp
+++ include/monopod_sdk/common_header.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "monopod_sdk/mode.hpp"
 #include <time_series/time_series.hpp>
 
 namespace monopod_drivers {
@@ -76,39 +77,6 @@ enum Measurements {
   encoder_index,
   measurement_count, // Meassurement count is the 'length' of the meassurement
                      // vector in motor board.
-};
-
-/**
- * @brief Mode defines the boards (joints) that are being considered from the
- * canbus. This allows us to handle say only wanting to use the leg or only
- * wanting to read plnarizer sensors. Also defines diferent task modes for the
- * gym environment.
- */
-enum class Mode {
-  /**
-   * @brief Complete free boom connector (5 joints total)
-   */
-  Free,
-  /**
-   * @brief Fixed boom connector (4 joints total)
-   */
-  Fixed_connector,
-  /**
-   * @brief Fixed boom connector and planrizer yaw (3 joints total)
-   */
-  Fixed,
-  /**
-   * @brief motor board
-   */
-  motor_board,
-  /**
-   * @brief encoder board 1
-   */
-  encoder_board1,
-  /**
-   * @brief encoder board 2
-   */
-  encoder_board2
 };
 
 /**
