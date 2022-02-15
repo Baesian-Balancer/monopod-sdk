f0ef7280d1624fba1e41370a329b077508f95043
Dawson Horvath <horvath.dawson@gmail.com>
2022-02-15 14:37:53 -0800

    print torque updates

--- src/motor_joint_module.cpp
+++ src/motor_joint_module.cpp
@@ -23,8 +23,6 @@ MotorJointModule::MotorJointModule(
 void MotorJointModule::set_torque(const double &desired_torque) {
 
   double desired_current = joint_torque_to_motor_current(desired_torque);
-  rt_printf("Desired Torque: %.3f, Desired Current: %.3f", desired_torque,
-            desired_current);
   // limit current to avoid overheating etc
   // ----------------------------------
   desired_current = std::min(desired_current, max_current_);
